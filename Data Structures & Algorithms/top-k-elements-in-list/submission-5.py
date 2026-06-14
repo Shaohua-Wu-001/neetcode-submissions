e582c9c2017c6a8a6efe60f811d70a48fe69152f
Andy S. Wu <197053324+Shaohua-Wu-001@users.noreply.github.com>
2026-06-14 19:54:31 +0800

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+
+        for num in nums:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+
+        # 用 bucket algorithm
+        # bucket 在這邊為出現的頻率
+
+        bucket = [[] for _ in range(len(nums) + 1)] #建立出頻率的bucket, 都是空list!
+
+        for num, freq in count.items():
+            bucket[freq].append(num) #放到對應頻率的bucket中
+
+        ans = []
+
+        for freq in range(len(bucket) - 1, 0, -1): #由大到小
+            for num in bucket[freq]:
+                ans.append(num)
+                if len(ans) == k: #前k個
+                    return ans
